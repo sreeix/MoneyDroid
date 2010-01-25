1fb53ff2df0ab2ee3f2df049d943fe04727ec417
Aneesh P U <aneeshpu@gmail.com>
2010-01-25 13:01:48 +0530

    refactored the receiver class

--- src/com/thoughtworks/SMSReceiver.java
+++ src/com/thoughtworks/SMSReceiver.java
@@ -8,28 +8,32 @@ import android.telephony.gsm.SmsMessage;
 import android.util.Log;
 import android.widget.Toast;
 
-public class SMSReceiver extends BroadcastReceiver{
+public class SMSReceiver extends BroadcastReceiver {
 
 	@Override
 	public void onReceive(Context ctx, Intent intent) {
-		Bundle bundle = intent.getExtras();        
-        SmsMessage[] msgs = null;
-        String str = "";            
-        if (bundle != null)
-        {
-            Object[] pdus = (Object[]) bundle.get("pdus");
-            msgs = new SmsMessage[pdus.length];            
-            for (int i=0; i<msgs.length; i++){
-                msgs[i] = SmsMessage.createFromPdu((byte[])pdus[i]);                
-                str += "SMS from " + msgs[i].getOriginatingAddress();                     
-                str += " :";
-                str += msgs[i].getMessageBody().toString();
-                str += "\n";        
-            }
-            Log.i("SMSAPP", str);
-            Toast.makeText(ctx, str, Toast.LENGTH_SHORT).show();
-        }                    
+		Log.i("SMS", "Received some SMS");
+		Bundle bundle = intent.getExtras();
+
+		if (bundle == null)
+			return;
+
+		final String str = makeSms(bundle);
+		Log.i("SMSAPP", str);
+	
+		Toast.makeText(ctx, str, Toast.LENGTH_SHORT).show();
+
+	}
+
+	private String makeSms(Bundle bundle) {
+		Object[] pdus = (Object[]) bundle.get("pdus");
+		StringBuilder str = new StringBuilder();
 		
+		for (int i = 0; i < pdus.length; i++) {
+			SmsMessage sms = SmsMessage.createFromPdu((byte[]) pdus[i]);
+			str.append("SMS from " + sms.getOriginatingAddress()).append(" :").append(sms.getMessageBody().toString()).append("\n");
+		}
+		return str.toString();
 	}
 
 }
