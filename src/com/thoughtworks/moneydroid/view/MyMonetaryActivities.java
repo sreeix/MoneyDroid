e911d962fefe46ef96016e7d02ab257a50fcfacb
Aneesh P U <aneeshpu@gmail.com>
2010-03-10 19:23:04 +0530

    added a root activity that displays a useless text

---
+++ src/com/thoughtworks/moneydroid/view/MyMonetaryActivities.java
@@ -0,0 +1,15 @@
+package com.thoughtworks.moneydroid.view;
+
+import com.thoughtworks.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MyMonetaryActivities extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+}
