461799764fed495abf50dc35b00906f9256c18dc
Aneesh P U <aneeshpu@gmail.com>
2010-03-10 19:58:29 +0530

    Cool changed the text in a text view

--- src/com/thoughtworks/moneydroid/view/MyMonetaryActivities.java
+++ src/com/thoughtworks/moneydroid/view/MyMonetaryActivities.java
@@ -1,9 +1,10 @@
 package com.thoughtworks.moneydroid.view;
 
-import com.thoughtworks.R;
-
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.thoughtworks.R;
 
 public class MyMonetaryActivities extends Activity{
 
@@ -11,5 +12,9 @@ public class MyMonetaryActivities extends Activity{
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+		
+		TextView myMonetaryViews = (TextView)findViewById(R.id.my_monetary_view);
+		myMonetaryViews.append("Hey I changed the text");
+		
 	}
 }
