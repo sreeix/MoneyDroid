6bf2a483b379d74516fb1a99e7d8a2f3716856cf
Aneesh P U <aneeshpu@gmail.com>
2010-03-14 12:50:59 +0530

    Displaying expenses in a list view'

--- src/com/thoughtworks/moneydroid/view/MyMonetaryActivities.java
+++ src/com/thoughtworks/moneydroid/view/MyMonetaryActivities.java
@@ -1,15 +1,12 @@
 package com.thoughtworks.moneydroid.view;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import android.app.ListActivity;
-import android.content.Context;
 import android.database.Cursor;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.ResourceCursorAdapter;
-import android.widget.TextView;
 
 import com.thoughtworks.R;
 import com.thoughtworks.moneydroid.transaction.ExpenseTracker;
@@ -51,58 +48,18 @@ public class MyMonetaryActivities extends ListActivity {
 		String[] projections = new String[] { Expense._EXPENSE_ID_WITH_TABLE, Expense._DATE, Expense._AMOUNT, Expense._BALANCE, Expense._VENDOR_ID, Vendor._NAME };
 		Cursor cursor = managedQuery(ExpenseTracker.CONTENT_URI, projections, null, null, Expense._DATE + " DESC");
 
-		// ListView listView =
-		// (ListView)findViewById(R.id.my_monetary_list_view);
-
-		final String[] COUNTRIES = new String[] { "Afghanistan", "Albania", "Algeria", "American Samoa", "Andorra", "Angola", "Anguilla", "Antarctica", "Antigua and Barbuda", "Argentina", "Armenia",
-				"Aruba", "Australia", "Austria", "Azerbaijan", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia and Herzegovina",
-				"Botswana", "Bouvet Island", "Brazil", "British Indian Ocean Territory", "British Virgin Islands", "Brunei", "Bulgaria", "Burkina Faso", "Burundi", "Cote d'Ivoire", "Cambodia",
-				"Cameroon", "Canada", "Cape Verde", "Cayman Islands", "Central African Republic", "Chad", "Chile", "China", "Christmas Island", "Cocos (Keeling) Islands", "Colombia", "Comoros",
-				"Congo", "Cook Islands", "Costa Rica", "Croatia", "Cuba", "Cyprus", "Czech Republic", "Democratic Republic of the Congo", "Denmark", "Djibouti", "Dominica", "Dominican Republic",
-				"East Timor", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Ethiopia", "Faeroe Islands", "Falkland Islands", "Fiji", "Finland",
-				"Former Yugoslav Republic of Macedonia", "France", "French Guiana", "French Polynesia", "French Southern Territories", "Gabon", "Georgia", "Germany", "Ghana", "Gibraltar", "Greece",
-				"Greenland", "Grenada", "Guadeloupe", "Guam", "Guatemala", "Guinea", "Guinea-Bissau", "Guyana", "Haiti", "Heard Island and McDonald Islands", "Honduras", "Hong Kong", "Hungary",
-				"Iceland", "India", "Indonesia", "Iran", "Iraq", "Ireland", "Israel", "Italy", "Jamaica", "Japan", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Kuwait", "Kyrgyzstan", "Laos",
-				"Latvia", "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg", "Macau", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta",
-				"Marshall Islands", "Martinique", "Mauritania", "Mauritius", "Mayotte", "Mexico", "Micronesia", "Moldova", "Monaco", "Mongolia", "Montserrat", "Morocco", "Mozambique", "Myanmar",
-				"Namibia", "Nauru", "Nepal", "Netherlands", "Netherlands Antilles", "New Caledonia", "New Zealand", "Nicaragua", "Niger", "Nigeria", "Niue", "Norfolk Island", "North Korea",
-				"Northern Marianas", "Norway", "Oman", "Pakistan", "Palau", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Pitcairn Islands", "Poland", "Portugal", "Puerto Rico",
-				"Qatar", "Reunion", "Romania", "Russia", "Rwanda", "Sqo Tome and Principe", "Saint Helena", "Saint Kitts and Nevis", "Saint Lucia", "Saint Pierre and Miquelon",
-				"Saint Vincent and the Grenadines", "Samoa", "San Marino", "Saudi Arabia", "Senegal", "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "Solomon Islands", "Somalia",
-				"South Africa", "South Georgia and the South Sandwich Islands", "South Korea", "Spain", "Sri Lanka", "Sudan", "Suriname", "Svalbard and Jan Mayen", "Swaziland", "Sweden",
-				"Switzerland", "Syria", "Taiwan", "Tajikistan", "Tanzania", "Thailand", "The Bahamas", "The Gambia", "Togo", "Tokelau", "Tonga", "Trinidad and Tobago", "Tunisia", "Turkey",
-				"Turkmenistan", "Turks and Caicos Islands", "Tuvalu", "Virgin Islands", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "United States",
-				"United States Minor Outlying Islands", "Uruguay", "Uzbekistan", "Vanuatu", "Vatican City", "Venezuela", "Vietnam", "Wallis and Futuna", "Western Sahara", "Yemen", "Yugoslavia",
-				"Zambia", "Zimbabwe" };
-		// setListAdapter(new ArrayAdapter<String>(this,
-		// android.R.layout.simple_list_item_1, COUNTRIES));
-		// listView.addHeaderView(textView);
-		// listView.addTouchables(arrayList);
-
-		setListAdapter(new ResourceCursorAdapter(this, android.R.layout.simple_list_item_1, cursor) {
-
-			@Override
-			public void bindView(View view, Context context, Cursor cursor) {
-				view.addTouchables(expenses(context, cursor));
-			}
-
-			private ArrayList<View> expenses(Context context, Cursor cursor) {
-				ArrayList<View> arrayList = new ArrayList<View>();
-				TextView textView = new TextView(context);
-				textView.setText("Foo Bar");
-				arrayList.add(textView);
-				while (cursor.moveToNext()) {
-					arrayList.add(oneExpense(context, cursor.getString(cursor.getColumnIndex(Vendor._NAME))));
-				}
-				return arrayList;
-			}
-
-			private TextView oneExpense(Context context, String vendor) {
-				TextView textView = new TextView(context);
-				textView.setText(vendor);
-				return textView;
-			}
-		});
+		List<String> expenses = new ArrayList<String>();
+		if(cursor.moveToFirst()){
+			
+			do{
+				expenses.add(expensePreview(cursor));
+			}while(cursor.moveToNext());
+		}
+		setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,expenses));
 		getListView().setTextFilterEnabled(true);
+	}
+
+	private String expensePreview(Cursor cursor) {
+		return String.format("%s:%s",cursor.getString(cursor.getColumnIndex(Vendor._NAME)), cursor.getString(cursor.getColumnIndex(Expense._AMOUNT)));
 	}
 }
